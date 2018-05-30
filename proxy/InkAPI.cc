4cd62377721e1697aa90f3921aa4a3cd00413b06
Jonathan Adams <jonathan.adams@clearswift.com>
2018-05-30 13:56:44 +0100

    Added a new API method.
    Method sets a parent proxy with "parent is proxy" option set to false.

--- proxy/InkAPI.cc
+++ proxy/InkAPI.cc
@@ -5775,6 +5775,19 @@ TSHttpTxnParentProxySet(TSHttpTxn txnp, const char *hostname, int port)
   sm->t_state.api_info.parent_proxy_port = port;
 }
 
+void TSHttpTxnParentOriginSet(TSHttpTxn txnp, const char *hostname, int port)
+{
+    sdk_assert(sdk_sanity_check_txn(txnp) == TS_SUCCESS);
+    sdk_assert(sdk_sanity_check_null_ptr((void * )hostname) == TS_SUCCESS);
+    sdk_assert(port > 0);
+
+    HttpSM *sm = (HttpSM *) txnp;
+
+    sm->t_state.api_info.parent_proxy_name = sm->t_state.arena.str_store(hostname, strlen(hostname));
+    sm->t_state.api_info.parent_proxy_port = port;
+    sm->t_state.parent_params->DefaultParent->parent_is_proxy = false;
+}
+
 TSReturnCode
 TSHttpTxnParentSelectionUrlGet(TSHttpTxn txnp, TSMBuffer bufp, TSMLoc obj)
 {
